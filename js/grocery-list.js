6e92ef7729e0cbb65915b45515f1ac8f2cd6d199
AggieWB <usmaggie23@gmail.com>
2020-06-07 22:49:25 -0700

    Move imgElement variable with createImgElement() call inside
    the for loop so that it creates a new image element on each array item
    Add function removeListItem() using the X image, and add a click event listener on the image

--- js/grocery-list.js
+++ js/grocery-list.js
@@ -3,10 +3,9 @@ function addListItems(event){
     var form = event.target;
     var items = form.querySelector('#items').value.split(',');
     var ulElement = document.querySelector('ul');
-
-    var imgElement = createXImgElement();
     
     for(var i = 0; i < items.length; i++){
+        var imgElement = createXImgElement();
         var liElement = document.createElement('li');
         liElement.textContent = items[i];
         liElement.appendChild(imgElement);
@@ -18,7 +17,15 @@ function createXImgElement(){
     var imgElement = document.createElement('img');
     imgElement.setAttribute('src', 'media/x-icon.png');
     imgElement.setAttribute('alt', 'A red x to indicate removal.');
+    imgElement.addEventListener('click', removeListItem);
     return imgElement;
+}
+
+function removeListItem(event){
+    var imgElement = event.target;
+    var liElement = imgElement.parentNode;
+    var ulElement = liElement.parentNode;
+    ulElement.removeChild(liElement);
 }
 
 document.querySelector('form').addEventListener('submit', addListItems);
