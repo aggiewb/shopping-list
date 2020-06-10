b9937e2781e5c50dd23b5aff7a800997e66980d5
AggieWB <usmaggie23@gmail.com>
2020-06-09 21:21:26 -0700

    Replace all single quotes with doubles quotes

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,35 +1,35 @@
 (function() {
-    'use strict';
-    var shoppingList = document.querySelector('ul');
+    "use strict";
+    var shoppingList = document.querySelector("ul");
 
     function addListItems(event){
         var form = event.target;
-        var items = form.querySelector('#items').value.split(',').map(function(item){
+        var items = form.querySelector("#items").value.split(",").map(function(item){
             return item.trim();
         });
 
         if(items[0].length > 0){
-            document.querySelector('#user-warning').textContent = "";
+            document.querySelector("#user-warning").textContent = "";
             for(var i = 0; (i < items.length) && (items[i].length > 0); i++){
                 appendItemToList(createXImgElement(), items[i]);
             }
         } else {
-            document.querySelector('#user-warning').textContent = "Please enter an item.";
+            document.querySelector("#user-warning").textContent = "Please enter an item.";
         }
         event.preventDefault();
         form.reset();
     }
 
     function createXImgElement(){
-        var imgElement = document.createElement('img');
-        imgElement.setAttribute('src', 'media/x-icon.png');
-        imgElement.setAttribute('alt', 'A red x to indicate removal.');
-        imgElement.addEventListener('click', removeListItem);
+        var imgElement = document.createElement("img");
+        imgElement.setAttribute("src", "media/x-icon.png");
+        imgElement.setAttribute("alt", "An X that can be clicked to remove this item from the list");
+        imgElement.addEventListener("click", removeListItem);
         return imgElement;
     }
 
     function appendItemToList(imgElement, item){
-        var liElement = document.createElement('li');
+        var liElement = document.createElement("li");
         liElement.textContent = item;
         liElement.appendChild(imgElement);
         shoppingList.appendChild(liElement);
@@ -47,9 +47,9 @@
         }
     }
 
-    document.querySelector('form').addEventListener('submit', addListItems);
-    document.querySelector('button').addEventListener('click', clearList);
+    document.querySelector("form").addEventListener("submit", addListItems);
+    document.querySelector("button").addEventListener("click", clearList);
 
     var date = new Date();
-    document.querySelector('footer').innerHTML = "Aggie Wheeler Bateman &copy; " +  date.getFullYear() + " <a href=\"https://www.aggiewheelerbateman.com\" target=\"_blank\">Personal Portfolio</a>";
+    document.querySelector("footer").innerHTML = "Aggie Wheeler Bateman &copy; " +  date.getFullYear() + " <a href=\"https://www.aggiewheelerbateman.com\" target=\"_blank\">Personal Portfolio</a>";
 })();
